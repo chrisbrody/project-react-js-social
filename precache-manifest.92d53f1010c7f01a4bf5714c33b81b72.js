b40e3f4921756508eabd4d91dd005b2effbb3b64
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-11 16:30:23 -0500

    Updates

---
+++ precache-manifest.92d53f1010c7f01a4bf5714c33b81b72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dad39d9159ada89cd280e0fd0ca223",
+    "url": "/project-react-js-social/index.html"
+  },
+  {
+    "revision": "5aff468340329ded6acc",
+    "url": "/project-react-js-social/static/js/2.5a4a925b.chunk.js"
+  },
+  {
+    "revision": "6a6837d1cd2aaed6d38a",
+    "url": "/project-react-js-social/static/js/main.4be8d701.chunk.js"
+  },
+  {
+    "revision": "43351b3bfdf815829266",
+    "url": "/project-react-js-social/static/js/runtime~main.88c6b2b4.js"
+  }
+]);
